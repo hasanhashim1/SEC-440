5911273b69d6650e2f2e9971d587851637a0e8a8
hasanhashim1 <hasan.hashim@mymail.champlain.edu>
2024-01-29 00:54:00 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,46 +1,45 @@
-#!/bin/vbash
+#!/bin/bash
 
-# Start configuration mode
-/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper begin
+# VyOS Configuration Script
 
-# Set system host-name
+# Enter configuration mode
+configure
+
+# Set hostname
 set system host-name vyos1-HasanHashim
 
-# Configure interfaces
+# Configure Ethernet interfaces
 set interfaces ethernet eth0 address 10.0.17.15/24
 set interfaces ethernet eth0 description "WAN"
 set interfaces ethernet eth1 address 10.0.5.2/24
 set interfaces ethernet eth1 description "LAN"
 
-# Configure static route
+# Set default gateway and DNS server
 set protocols static route 0.0.0.0/0 next-hop 10.0.17.2
 set system name-server 10.0.17.2
 
-# Commit and save
-commit
-save
-
-# Configure NAT source rule
+# NAT Rules
 set nat source rule 10 outbound-interface eth0
 set nat source rule 10 source address 10.0.5.0/24
 set nat source rule 10 description "Lan to Wan"
 set nat source rule 10 translation address masquerade
 
-# Commit and save
-commit
-save
-
-# Configure DNS forwarding and SSH
+# DNS Forwarding
 set service dns forwarding listen-address 10.0.5.1
 set service dns forwarding allow-from 10.0.5.0/24
 set service dns forwarding system
+
+# SSH Service
 set service ssh listen address 0.0.0.0
 
-# Commit and save
+# Commit and save configuration
 commit
 save
 
 # Exit configuration mode
-/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper end
+exit
 
-echo "Configuration applied successfully."
+# Display NAT rule 10, services configuration, and test connectivity
+run show nat source rule 10
+run show service
+run ping google.com
