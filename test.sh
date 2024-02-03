3de03fc1efbe0293b15771424e4f5e4eef12de7f
hasanhashim1 <hasan.hashim@mymail.champlain.edu>
2024-02-02 22:27:37 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,26 +4,19 @@ source /opt/vyatta/etc/functions/script-template
 /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper begin
 
 # Set system host-name
-conf
-set nat destination rule 10 description "WEB01 on WAN (HTTP)"
-set nat destination rule 10 destination port 80
-set nat destination rule 10 inbound-interface eth0
-set nat destination rule 10 protocol tcp
-set nat destination rule 10 translation address 10.0.5.100
-set nat destination rule 10 translation port 80
+set high-availability vrrp group WAN vrid 10
+set high-availability vrrp group WAN interface eth0
+set high-availability vrrp group WAN priority '200'
+set high-availability vrrp group WAN address 10.0.17.105/24
 
-set nat destination rule 20 description "WEB01 on WAN (SSH)"
-set nat destination rule 20 destination port 22
-set nat destination rule 20 inbound-interface eth0
-set nat destination rule 20 protocol tcp
-set nat destination rule 20 translation address 10.0.5.100
-set nat destination rule 20 translation port 22
+set high-availability vrrp group LAN vrid 20
+set high-availability vrrp group LAN interface eth1
+set high-availability vrrp group LAN priority '200'
+set high-availability vrrp group LAN address 10.0.5.1/24
 
-
+show high-availability vrrp
 commit
 save
-show nat destination rule 10
-show nat destination rule 20
 
 # Exit configuration mode
 /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper end
