deab40086971eb183b57c677a10df167b82bba06
hasanhashim1 <hasan.hashim@mymail.champlain.edu>
2024-02-24 16:32:29 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,81 +1,36 @@
 #!/bin/bash
 
-# Define variables for MySQL connection
-DB_SERVER="10.0.5.201" # Change to your MySQL server IP
-DB_USERNAME="root" # Change to your MySQL username
-DB_PASSWORD="Passw0rd!" # Change to your MySQL password
-DB_NAME="cats" # Change to your database name
+# MySQL/MariaDB root password
+ROOT_PASSWORD='Passw0rd!' # Change this to your MySQL root password
+DB_NAME='cats'
+DB_USER='cat_user'
+DB_PASSWORD='Passw0rd!' # Change this to a secure password
 
-# Install PHP and necessary extensions
-echo "Installing PHP and necessary extensions..."
-sudo yum install -y php php-mysqlnd
+# SQL statements
+SQL_STATEMENTS="
+CREATE DATABASE IF NOT EXISTS ${DB_NAME};
+CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
+FLUSH PRIVILEGES;
 
-# Set SELinux boolean for HTTPD to connect to database
-echo "Setting SELinux boolean for HTTPD to connect to database..."
-sudo setsebool -P httpd_can_network_connect_db on
+USE ${DB_NAME};
 
-# Restart HTTPD service to apply changes
-echo "Restarting HTTPD service..."
-sudo systemctl restart httpd.service
+CREATE TABLE IF NOT EXISTS tabbies (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(150) NOT NULL,
+    age VARCHAR(50) NOT NULL,
+    birthday VARCHAR(150) NOT NULL,
+    weight VARCHAR(50) NOT NULL,
+    fur_color VARCHAR(100) NOT NULL,
+    description TEXT
+);
 
-# Create PHP info file
-echo "Creating PHP info file..."
-cat <<EOF | sudo tee /var/www/html/info.php
-<?php
-phpinfo();
-?>
-EOF
+INSERT INTO tabbies (name, age, birthday, weight, fur_color, description) VALUES
+('Whiskers', '3 years', '2019-07-18', '10 lbs', 'Black and white', 'Very playful and loves to nap in the sun.'),
+('Shadow', '5 years', '2018-05-24', '12 lbs', 'Gray', 'Shy at first, but very affectionate once she warms up to you.');
+"
 
-# Create HTML form and PHP script for database interaction
-echo "Creating HTML form and PHP script for database interaction..."
-# HTML form
-cat <<EOF | sudo tee /var/www/html/index.html
-<html>
-<body>
-<h1>Cat Info Database</h1>
-<form action="cats.php" method="post">
-Which cat do you want to know about? (Options: Whiskers, Shadow)</br>
-Cat name: <input type="text" name="name"></br>
-<input type="submit">
-</form>
-</body>
-</html>
-EOF
+# Execute SQL statements
+echo "${SQL_STATEMENTS}" | mysql -u root -p"${ROOT_PASSWORD}"
 
-# PHP script
-cat <<EOF | sudo tee /var/www/html/cats.php
-<?php
-\$servername = "$DB_SERVER";
-\$username = "$DB_USERNAME";
-\$password = "$DB_PASSWORD";
-\$dbname = "$DB_NAME";
-
-// Get name from HTML form
-\$name = \$_POST['name'];
-
-// Connect to DB
-\$connect = new mysqli(\$servername, \$username, \$password, \$dbname);
-if (\$connect->connect_error) {
-    die("Connection failed: " . \$connect->connect_error);
-}
-
-// SQL query
-\$query = "SELECT * FROM tabbies WHERE name = '".\$name."'";
-\$output = \$connect->query(\$query) or die(\$connect->error.__LINE__);
-if (\$output->num_rows > 0) {
-    while(\$row = \$output->fetch_assoc()) {
-        echo "Name: " . \$row["name"] . "<br>";
-        echo "Age: " . \$row["age"] . "<br>";
-        echo "Birthday: " . \$row["birthday"] . "<br>";
-        echo "Weight: " . \$row["weight"] . "<br>";
-        echo "Fur Color: " . \$row["fur_color"] . "<br>";
-        echo "Description: " . \$row["description"] . "<br><br>";
-    }
-} else {
-    echo "No results";
-}
-\$connect->close();
-?>
-EOF
-
-echo "Setup complete. Visit http://your_server_ip/info.php and http://your_server_ip/index.html to check."
+echo "Database and user setup complete."
